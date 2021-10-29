37bd4314d7a0332e096b463f2de5eac8a8021920
Irit Kushwaha <67689773+irit2607@users.noreply.github.com>
2021-10-29 21:47:38 +0530

    Delete config directory

--- config/mongoURI.js
+++
@@ -1,1 +0,0 @@
-module.exports = "mongodb://irit26:iriteshu@cluster0-shard-00-00.6dy1n.mongodb.net:27017,cluster0-shard-00-01.6dy1n.mongodb.net:27017,cluster0-shard-00-02.6dy1n.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-s6d5o4-shard-0&authSource=admin&retryWrites=true&w=majority";
