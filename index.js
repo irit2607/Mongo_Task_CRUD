30925725bece1fffc3a4790ead097543dc688f63
Irit Kushwaha <iritkushwaha2607@gmail.com>
2021-10-29 18:31:33 +0530

    installed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./controllers/routes');
-
+const mongoose = require('mongoose');
+const uri = require('./config/mongoURI');
 const app = express();
 
 app.use(express.urlencoded({ extended: true},),);
@@ -9,6 +10,9 @@ app.set('view engine','ejs');
 app.set('views', __dirname + '/views');
 
 app.use(routes);
+
+//connect to mongo
+mongoose.connect(uri, { useNewUrlParser :true, useUnifiedTopology :true, useFindAndModify: false, useCreateIndex : true, }).then(() => console.log("Connected !"),);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log("Started on PORT : " + PORT,),);
