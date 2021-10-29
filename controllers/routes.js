ef934e6206976c2542045173515491315aa37a48
Irit Kushwaha <iritkushwaha2607@gmail.com>
2021-10-29 21:44:00 +0530

    done

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,7 +1,48 @@
 const router = require('express').Router();
+const tasks = require('../modals/taskModal');
 
 router.get('/',(req,res) => {
-    res.send('hi');
+    var mytasks;
+    tasks.find({}, (err, data) => {
+        if(err)
+        {
+            console.log(err);
+        }
+        if(data)
+        {
+            mytasks = data;
+        }
+        res.render("index", {data : mytasks});
+    });
+});
+
+router.post('/add',(req,res) => {
+
+    const task = req.body.task;
+    tasks({ task: task }).save(function(err, doc) {
+        if(err)
+        {
+            console.log(err);
+        }
+        res.redirect('/');
+    });
+
+});
+
+router.post('/delete',(req,res) => {
+    const id  = req.body.id;
+
+    tasks.findByIdAndRemove({ _id : id}, (err, doc) => {
+        res.redirect('/');
+    });
+});
+
+router.post('/update',(req,res) => {
+    const id = req.body.id;
+
+    tasks.findByIdAndUpdate({ _id : id}, {status : true}, (err,doc) => {
+        res.redirect('/');
+    });
 });
 
 module.exports = router;
